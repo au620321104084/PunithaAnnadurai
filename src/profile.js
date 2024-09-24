2bdbff10440bdc974899004a94a205a1ce026ae2
Punitha Annadurai <punithaannadurai321@gmail.com>
2024-09-24 12:22:26 +0530

    first commit

--- src/profile.js
+++ src/profile.js
@@ -4,11 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>narmadha</h1>
+      <h1>name:narmadha</h1>
       <h1>age:20</h1>
-      <h1>dob:14-04-2004</h1>
-      <h1>skill:html</h1>
-      
+      <h2>dob:14-04-2004</h2>
+      <h1>address:kallakurichi</h1>
     </div>
   );
 }
